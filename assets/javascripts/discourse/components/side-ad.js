c0a48b85b3333ce32d10a5f335267512f84a29d9
neha01kbizsoft <neha.kbizsoft@gmail.com>
2023-11-24 17:35:20 +0530

    exco-player

--- assets/javascripts/discourse/components/side-ad.js
+++ assets/javascripts/discourse/components/side-ad.js
@@ -61,4 +61,53 @@ export default AdComponent.extend({
       showOnCurrentPage
     );
   }
+
+addExcoPlayer() {
+    const currentUser = this.get("currentUser");
+
+    if (currentUser && currentUser.username) {
+      let valueExists = true;
+
+      for (const obj of currentUser.groups) {
+        if (
+          obj.name === "admins" ||
+          obj.name === "Pro-Members" ||
+          obj.name === "Business-Member" ||
+          obj.name === "Pro-Fighters" ||
+          obj.name === "Black-Belts" ||
+          obj.name === "Mod-Team" ||
+          obj.name === "OG-Mods" ||
+          obj.name === "Top-Men"
+        ) {
+          valueExists = false;
+          break; 
+        }
+      }
+
+      if (valueExists) {
+        setTimeout(() => {
+          Ember.$(".video_section").html('');
+          Ember.$('<div class="video_section"><div id="ace0fe48-0bdb-4202-b78c-dafca2c16291"></div></div>').insertAfter(".side-ad.discourse-adplugin");
+        }, 1000);
+      }
+    }
+  },
+
+  
+  didInsertElement() {
+    this._super(...arguments);
+
+   
+    Ember.$.getScript("https://player.ex.co/player/ace0fe48-0bdb-4202-b78c-dafca2c16291")
+      .done(() => {
+        
+        this.addExcoPlayer();
+      })
+      .fail((jqxhr, settings, exception) => {
+       
+        console.error("Failed to load Ex.co player script:", exception);
+      });
+  },
+
+  
 })
