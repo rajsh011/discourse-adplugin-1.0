a7576e3d81a44802d5e2eacd0325dc679bcf6b1a
neha01kbizsoft <neha.kbizsoft@gmail.com>
2023-11-24 18:42:10 +0530

    exco-player

--- assets/javascripts/discourse/components/ad-slot.js
+++ assets/javascripts/discourse/components/ad-slot.js
@@ -267,3 +267,11 @@ export default AdComponent.extend({
     const currentUser = Discourse.User.current();
     var valueExists = true;
     console.log(valueExists);
+
+    if(valueExists==true){
+  
+    setTimeout(function() {
+      $(".video_section").html('');   
+      $('<div class="video_section"><div id="ace0fe48-0bdb-4202-b78c-dafca2c16291"></div></div>').insertAfter(".side-ad-outlet.discourse-adplugin");
+    }, 1000);   
+  }
